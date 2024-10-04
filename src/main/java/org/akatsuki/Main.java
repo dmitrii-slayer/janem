6bede4656802f73184705d52a2648ceec477382d
Dmitrii Vartik <dekesite@mail.ru>
2024-10-04 08:27:09 +0300

    first commit

---
+++ src/main/java/org/akatsuki/Main.java
@@ -0,0 +1,10 @@
+package org.akatsuki;
+
+import org.janusgraph.graphdb.server.JanusGraphServer;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JanusGraphServer.main(args);
+    }
+}
